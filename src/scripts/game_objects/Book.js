2c40aaf13bf144f8b831a4bd9bf8ab7e191e31f7
Gabriel Edwards <gabrieledwards@ymail.com>
2016-12-11 15:23:35 -0800

    added burn functions in book.js but did not implement

--- src/scripts/game_objects/Book.js
+++ src/scripts/game_objects/Book.js
@@ -5,7 +5,13 @@ export default class Book extends Phaser.Sprite{
     this.body.immovable = true;
     this.body.height = 2;
     this.animations.add('open', [0, 1, 2, 3], 12, false);
+    this.animations.add('stayOpen', [3], 12, false);
     this.animations.add('close', [4, 5, 6, 7], 12, false);
+    this.animations.add('stayClosed', [7], 12, false);
+    this.animations.add('burnClosed', [8, 9], 12, true);
+    this.animations.add('burnOpen', [10, 11], 12, true);
+
+    this.playerPresent = false;
   }
 
   open () {
@@ -15,4 +21,23 @@ export default class Book extends Phaser.Sprite{
   close () {
     this.animations.play('close');
   }
+
+  burn () {
+    if (this.playerPresent == true) {
+      this.animations.play('burnOpen');
+    }
+    else {
+      this.animations.play('burnClosed');
+    }
+  }
+
+  stopBurning () {
+    if (this.playerPresent == true) {
+      this.animations.play('stayOpen');
+    }
+    else {
+      this.animations.play('stayClosed');
+    }
+  }
+
 }
