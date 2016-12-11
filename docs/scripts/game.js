aebc6e52f71c399f585916047793397af6140553
Scott Grogan <sgrogan@navigatingcancer.com>
2016-12-11 14:54:39 -0800

    Tweaking spell bounding box

--- docs/scripts/game.js
+++ docs/scripts/game.js
@@ -1144,7 +1144,7 @@
 	
 	    game.physics.enable(_this);
 	
-	    _this.anchor.x = 0;
+	    _this.anchor.x = .5;
 	    _this.anchor.y = .5;
 	    _this.animations.add('cast', [1, 2, 3], 12, false);
 	    return _this;
@@ -1158,21 +1158,25 @@
 	      switch (facing) {
 	        case LEFT:
 	          this.angle = 180;
+	          this.body.setSize(4, 29, 2, 10);
 	          this.body.velocity.x = -200;
 	          this.body.velocity.y = 0;
 	          break;
 	        case RIGHT:
 	          this.angle = 0;
+	          this.body.setSize(4, 29, 9, 8);
 	          this.body.velocity.x = 200;
 	          this.body.velocity.y = 0;
 	          break;
 	        case UP:
 	          this.angle = 270;
+	          this.body.setSize(29, 4, -8, 18);
 	          this.body.velocity.x = 0;
 	          this.body.velocity.y = -200;
 	          break;
 	        case DOWN:
 	          this.angle = 90;
+	          this.body.setSize(29, 4, -6, 25);
 	          this.body.velocity.x = 0;
 	          this.body.velocity.y = 200;
 	          break;
--- src/scripts/game_objects/Spell.js
+++ src/scripts/game_objects/Spell.js
@@ -9,7 +9,7 @@ export default class Spell extends Phaser.Sprite {
 
     game.physics.enable(this);
 
-    this.anchor.x = 0;
+    this.anchor.x = .5;
     this.anchor.y = .5;
     this.animations.add('cast', [1, 2, 3], 12, false);
   }
@@ -20,21 +20,25 @@ export default class Spell extends Phaser.Sprite {
     switch (facing) {
       case LEFT:
         this.angle = 180;
+        this.body.setSize(4, 29, 2, 10)
         this.body.velocity.x = -200;
         this.body.velocity.y = 0;
         break;
       case RIGHT:
         this.angle = 0;
+        this.body.setSize(4, 29, 9, 8)
         this.body.velocity.x = 200;
         this.body.velocity.y = 0;
         break;
       case UP:
         this.angle = 270;
+        this.body.setSize(29, 4, -8, 18)
         this.body.velocity.x = 0;
         this.body.velocity.y = -200;
         break;
       case DOWN:
         this.angle = 90;
+        this.body.setSize(29, 4, -6, 25)
         this.body.velocity.x = 0;
         this.body.velocity.y = 200;
         break;
